ecbc416edcb759e776e56b00f0fcd9e2ac9bebef
Prince <princekumarsingh016592@gmail.com>
2026-08-12 21:32:51 +0530

    v1

---
+++ Portfolio.java
@@ -0,0 +1,16 @@
+
+
+public class Portfolio {
+  public static void main(String a[]) {
+    int age = 22;
+    double salary = 22500.50;
+    char gender = 'M';
+    boolean married = true;
+    String name = "Prince";
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + age);
+    System.out.println("Salary: " + salary);
+    System.out.println("Gender: " + gender);
+    System.out.println("Married: " + married);
+  }
+}
